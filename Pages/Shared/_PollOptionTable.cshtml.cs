19bec5f73e51b4f72c2a825b80faa3a0ee49fa9c
Philipp Meier <philipp.meier@gmx.at>
2023-02-17 19:34:20 +0100

    fix: Removed input checkbox from the view form.

---
+++ Pages/Shared/_PollOptionTable.cshtml.cs
@@ -0,0 +1,16 @@
+using Edap.Models;
+
+namespace Edap.Shared;
+
+public class _PollOptionTableModel {
+    public Poll Poll { get; init; }
+    public bool IsReadOnly { get; init; }
+    
+    public static _PollOptionTableModel Create(Poll poll, bool isReadOnly = false)
+        => new _PollOptionTableModel(poll, isReadOnly);
+    
+    private _PollOptionTableModel(Poll poll, bool isReadOnly) {
+        Poll = poll;
+        IsReadOnly = isReadOnly;
+    }
+}
